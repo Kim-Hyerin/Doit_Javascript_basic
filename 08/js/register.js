520f7f72e8e3396cc6f6ed0e772ac3825ac4f520
Kim-Hyerin <kor99lyn@naver.com>
2021-06-20 17:05:09 +0900

    DOM을 사용한 참가 신청 명단 프로그램

--- 08/js/register.js
+++ 08/js/register.js
@@ -9,17 +9,18 @@ function newRegister() {
     delBttn.setAttribute("class", "del"); //버튼에 class = "del" 속성 설정
     delBttn.appendChild(delText); //텍스트 노드를 버튼 요소의 자식 요소로 추가
     newP.appendChild(delBttn); //del 버튼을 p 요소의 자식 요소로 추가
-    
+
     var nameList = document.querySelector("#nameList");
     nameList.insertBefore(newP, nameList.childNodes[0]); //p 요소를 #nameList 맨 앞에 추가하기 (맨 위에 이름 추가)
     //nameList.appendChild(newP); //newP 노드를 nameList 노드의 자식 노드로 연결 (맨 아래에 이름 추가)
     userName.value = ""; //다음 이름 입력할 수 있도록 텍스트 필드 비우기
 
     var removeBttns = document.querySelectorAll(".del");
-    for(var i=0; i<removeBttns.length; i++){
-        removeBttns[i].addEventListener("click", function(){
-            if(this.parentNode.parentNode) // span->p->nameList
-                this.parentNode.parentNode.removeChild(this.parentNode);
+
+    for(var i=0; i<removeBttns.length; i++){ //removeBttns에 있는 요소 전체를 반복
+        removeBttns[i].addEventListener("click", function(){ //i번째 버튼을 클릭했을 때 실행할 함수 선언
+            if(this.parentNode.parentNode) // span->p->nameList , 현재 노드(this)의 부모 노드의 부모 노드가 있을 경우 실행
+                this.parentNode.parentNode.removeChild(this.parentNode); //‘현재 노드(this)의 부모 노드의 부모 노드’를 찾아 ‘현재 노드(this)의 부모 노드(p 노드)’ 삭제
         });
     }
 }
